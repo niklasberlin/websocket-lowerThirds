cca4025525bf38a47dff5b7f5718aa9b8bfcc681
pajowu <git@ca.pajowu.de>
2021-12-05 14:32:43 +0100

    🍱 Move assets to asset-folder

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
-from tornado import websocket, web, ioloop
 import json
+from pathlib import Path
+
+from tornado import ioloop, web, websocket
+
+ASSET_PATH = Path("./assets")
 
 cl = []
 
+
 class IndexHandler(web.RequestHandler):
     def get(self):
-        self.render("index-new.html")
+        self.render(str(ASSET_PATH / "index-new.html"))
+
 
 class ControlHandler(web.RequestHandler):
     def get(self):
-        self.render("control.html")
+        self.render(str(ASSET_PATH / "control.html"))
+
 
 class SocketHandler(websocket.WebSocketHandler):
     def check_origin(self, origin):
@@ -23,8 +30,8 @@ class SocketHandler(websocket.WebSocketHandler):
         if self in cl:
             cl.remove(self)
 
-class ApiHandler(web.RequestHandler):
 
+class ApiHandler(web.RequestHandler):
     @web.asynchronous
     def get(self, *args):
         self.finish()
@@ -33,7 +40,7 @@ class ApiHandler(web.RequestHandler):
         lineOne = self.get_argument("l1")
         lineTwo = self.get_argument("l2")
         delay = self.get_argument("d")
-        
+
         ##check for datatype and plausible values
 
         ##convert string to int for delay
@@ -49,10 +56,7 @@ class ApiHandler(web.RequestHandler):
         if not isinstance(lineTwo, str):
             lineTwo = ""
 
-
-
-        
-        data = {"lineOne": lineOne, "lineTwo" : lineTwo, "delay":delay}
+        data = {"lineOne": lineOne, "lineTwo": lineTwo, "delay": delay}
         data = json.dumps(data)
         for c in cl:
             c.write_message(data)
@@ -61,23 +65,26 @@ class ApiHandler(web.RequestHandler):
     def post(self):
         pass
 
-app = web.Application([
-    (r'/', IndexHandler),
-    (r'/ws', SocketHandler),
-    (r'/api', ApiHandler),
-    (r'/(favicon.ico)', web.StaticFileHandler, {'path': '../'}),
-    (r'/(style.css)', web.StaticFileHandler, {'path': './'}),
-    (r'/(rest_api_example.png)', web.StaticFileHandler, {'path': './'}),
-    (r'/(logo.png)', web.StaticFileHandler, {'path': './'}),
-    (r'/control.html', ControlHandler),
-    (r'/control', ControlHandler),
-    (r'/(control.css)', web.StaticFileHandler, {'path': './'}),
-    (r'/(control.js)', web.StaticFileHandler, {'path': './'}),
-    (r'/(jquery.js)', web.StaticFileHandler, {'path': './'}),
-    (r'/(PoliticsHead.otf)', web.StaticFileHandler, {'path': './'}),
-])
 
-if __name__ == '__main__':
+app = web.Application(
+    [
+        (r"/", IndexHandler),
+        (r"/ws", SocketHandler),
+        (r"/api", ApiHandler),
+        (r"/(favicon.ico)", web.StaticFileHandler, {"path": ASSET_PATH}),
+        (r"/(style.css)", web.StaticFileHandler, {"path": ASSET_PATH}),
+        (r"/(rest_api_example.png)", web.StaticFileHandler, {"path": ASSET_PATH}),
+        (r"/(logo.png)", web.StaticFileHandler, {"path": ASSET_PATH}),
+        (r"/control.html", ControlHandler),
+        (r"/control", ControlHandler),
+        (r"/(control.css)", web.StaticFileHandler, {"path": ASSET_PATH}),
+        (r"/(control.js)", web.StaticFileHandler, {"path": ASSET_PATH}),
+        (r"/(jquery.js)", web.StaticFileHandler, {"path": ASSET_PATH}),
+        (r"/(PoliticsHead.otf)", web.StaticFileHandler, {"path": ASSET_PATH}),
+    ]
+)
+
+if __name__ == "__main__":
     print("starting server on Port 8888")
     app.listen(8888)
     ioloop.IOLoop.instance().start()
